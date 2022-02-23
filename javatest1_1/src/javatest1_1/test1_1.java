b579c8650684788c1f9e79580692ae7d14793900
minzzung <dullini0205@gmail.com>
2022-02-23 22:57:04 +0900

    add test2_1

--- javatest1_1/src/javatest1_1/test1_1.java
+++ javatest1_1/src/javatest1_1/test1_1.java
@@ -1,32 +1,35 @@
 package javatest1_1;
 
+
 import java.util.Scanner;
+
 public class test1_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		System.out.println("정수 3개를 입력하시오 : ");
-		int a = in.nextInt();
-		int b = in.nextInt();
-		int c = in.nextInt();
+		System.out.println("영문자를 입력하십시오: ");
+		while(in.hasNext())
+		{
+			String strIn = in.next();
+			char chIn = strIn.charAt(0);
+			if(((chIn)>='a'&&(chIn)<='z')||((chIn)>='A'&&(chIn)<='Z'))
+			{
+				if(((chIn)>='a'&&(chIn)<='z'))
+				{
+					chIn -= 32;
+				}
+				else
+				{
+					chIn += 32;
+				}
+				System.out.println("변환된 문자 : "+chIn);
+			}
+			else
+				System.out.println("영문자가 아닙니다.다시 입력해 주십시오.");
+			
 		
-		if(a+b<=c) {
-			System.out.println("삼각형이 만들어지지 않습니다.");
 		}
-		else if(b+c<=a) {
-			System.out.println("삼각형이 만들어지지 않습니다.");
-		}
-		else if(c+a<=b) {
-			System.out.println("삼각형이 만들어지지 않습니다.");
-		}
-		else {
-			double result = 0;
-			double s =(a+b+c)*0.5;
-			result = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-			System.out.printf("삼각형의 넓이는 %f입니다.\n",result);
-		}
-
 	}
 
 }
---
+++ javatest2_1/src/javatest2_1/test2_1.java
@@ -0,0 +1,20 @@
+package javatest2_1;
+
+public class test2_1 {
+	static void printMultTable(int argI)
+	{
+		for(int x = 1; x <= argI ; x++)
+		{
+			for(int y = 1; y <= x ; y++)
+			{
+				System.out.print(x*y+" ");
+			}
+			System.out.printf("\n");
+		}
+		return;
+	}
+	public static void main (String args[])
+	{
+		printMultTable(7);
+	}
+}
